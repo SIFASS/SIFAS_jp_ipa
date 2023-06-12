61b83e1d38ddd8e4b21737d93832842b312b5b89
Misty <gyc990326@gmail.com>
2023-06-12 19:39:17 +0800

    Switching to new decrypt infra

--- workflow_helper/decrypt_binary.py
+++ workflow_helper/decrypt_binary.py
@@ -11,64 +11,54 @@ from zipfile import ZipFile, Path
 
 
 ipa_file = sys.argv[1]
-with ZipFile(ipa_file, 'r') as ipa:
-    app_name = None
-    for i in ipa.filelist:
-        r = re.search(r"^Payload/(.+?)\.app/$", i.filename)
-        if r:
-            app_name = r.group(1)
-            break
-    if not app_name:
-        raise Exception("*.app folder not found in ipa!")
-        
-    with ipa.open("Payload/{}.app/Info.plist".format(app_name), "r") as f:
-        info_plist = plistlib.load(f)
-        binary_name = info_plist["CFBundleExecutable"]
-        identifier = info_plist["CFBundleIdentifier"]
 
-    with ipa.open("Payload/{}.app/{}".format(app_name, binary_name), "r") as f:
-        binary = f.read()
+import requests
+import time
 
-    
-    ipazip_io = BytesIO()
-    with ZipFile(ipazip_io, "w") as ipazip:
-        def addfile(fn):
-            with ipa.open(fn, "r") as f:
-                with ipazip.open(fn, "w") as nf:
-                    nf.write(f.read())
-        addfile("Payload/{}.app/Info.plist".format(app_name))
-        addfile("Payload/{}.app/{}".format(app_name, binary_name))
-        for path in Path(ipa, "Payload/{}.app/SC_Info/".format(app_name)).iterdir():
-            addfile(path.at)
-
-    bundleID = "{}/{}.app/{}".format(identifier, app_name, binary_name)
+HOST = os.environ["NEW_DECRYPT_SERVER"]
 
 # push to server
-DECRYPT_SERVER = os.environ["DECRYPT_SERVER"]
-DECRYPT_SERVER_TOKEN = os.environ["DECRYPT_SERVER_TOKEN"]
-print("Uploading to server...")
-r = requests.post("https://{}/submit".format(DECRYPT_SERVER), files={
-    "token": DECRYPT_SERVER_TOKEN,
-    "binary": ipazip_io.getvalue(),
-    "sc_info": bundleID,
-    "hash": sha256(ipazip_io.getbuffer()).hexdigest()
+ipa_abs_path = os.path.abspath(ipa_file)
+r = requests.post(HOST + '/add_http_decrypt_task', json={
+    'HTTPUrl': f"https://ghactions-tmp1.misty.moe/chfs/shared/{ipa_abs_path}",
+    'HTTPMethod': 'GET',
+    'StartOffset': 0,
+    'FileSize': -1,
+    'RemotePath': f'dropbox-misty:ios-decrypt/testdec-{int(time.time())}.tar',
 })
-print(r)
-task_id = int(r.text)
-assert r.status_code == 200
+taskret = r.json()
+if taskret['status'] != 'ok':
+    print("Failed to create task: ", taskret)
 
-# wait for result
-result_url = "https://{}/upload/{}/decrypted".format(DECRYPT_SERVER, task_id)
-while requests.head(result_url).status_code == 404:
-    print("Waiting for decryption...")
+task_id = taskret['task_id']
+task_queue = taskret['task_queue']
+print("Got decrypt task: id %s queue %s" % (task_id, task_queue))
+
+print("Waiting for task completion...")
+while True:
+    r = requests.post(HOST + '/get_task_data', json={
+        'task_id': task_id,
+        'task_queue': task_queue,
+        'need_download': False,
+    })
+    ret = r.json()
+    if ret['status'] == 'error':
+        print("Failed to get task status: ", ret)
+        break
+    elif ret['status'] == 'pending':
+        print("waiting for task finish...")
+    elif ret['status'] == 'ok':
+        print("success! ret: ", ret['decrypted_result'])
+        savedPath = ret['decrypted_result']['SavedPath']
+        savedSize = ret['decrypted_result']['SavedSize']
+        r = requests.post(HOST + '/get_task_data', json={
+            'task_id': task_id,
+            'task_queue': task_queue,
+            'need_download': True,
+        })
+        with open('dec.tar', 'wb') as f:
+            f.write(r.content)
+        assert len(r.content) == savedSize
+        break
+
     time.sleep(10)
-
-print("Downloading decrypted binary...")
-result = requests.get(result_url)
-assert result.status_code == 200
-if result.content.startswith(b"error"):
-    print("Decryption Error: %s" % result.text)
-    sys.exit(1)
-with open(binary_name, "wb") as f:
-    f.write(result.content)
-print("All success.")
